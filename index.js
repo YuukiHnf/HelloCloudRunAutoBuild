41a80bf5b359411739bcc913335116220f4cceaf
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-04-23 16:46:55 +0900

    test auto build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello GCP Cloud Run!!!");
+  res.send("Hello GCP Cloud Run with auto build!!!");
 });
 
 const port = process.env.port || 8080;
